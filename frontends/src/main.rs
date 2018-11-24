0e8fed46ffb39eac8ac055f71726bf0c4d8c7134
e-matteson <e.r.matteson@gmail.com>
2018-11-23 21:39:25 -0500

    feat: add demo of terminal frontend features

---
+++ frontends/src/main.rs
@@ -0,0 +1,142 @@
+use std::io;
+
+use termion::event::Key;
+
+use frontends::{Event, Frontend, Terminal};
+use pretty::{Color, ColorTheme, Pos, Row, Style};
+
+fn main() -> Result<(), io::Error> {
+    let mut demo = TermDemo::new(20)?;
+    demo.intro()?;
+    demo.run()?;
+    drop(demo);
+    println!("Exited alternate screen. Your cursor should be visible again.");
+    Ok(())
+}
+
+/// Demonstrate basic features of the terminal frontend.
+struct TermDemo {
+    /// How many events to handle before exiting.
+    num_events: usize,
+    /// The current line to print text to.
+    line: Row,
+    /// The underlying terminal.
+    term: Terminal,
+}
+
+impl TermDemo {
+    fn new(num_events: usize) -> Result<Self, io::Error> {
+        Ok(Self {
+            num_events,
+            line: 0,
+            term: Terminal::new(ColorTheme::colorful_hexagon())?,
+        })
+    }
+
+    /// Print an intro message explaining how the demo will work.
+    fn intro(&mut self) -> Result<(), io::Error> {
+        self.println(
+            "This is a demo of printing to the terminal.",
+            Style::plain(),
+        )?;
+        self.println("Click to paint!", Style::color(Color::Red))?;
+        self.println(
+            "Type q to quit, c to clear screen, or s to print size.",
+            Style::color(Color::Yellow),
+        )?;
+        self.println(
+            "Or type something else to print its event below.",
+            Style::color(Color::Green),
+        )?;
+        self.println(
+            &format!(
+                "The demo will end after {} keypresses or clicks.",
+                self.num_events
+            ),
+            Style::color(Color::Blue),
+        )
+    }
+
+    /// Respond to some key/mouse events, then quit.
+    fn run(&mut self) -> Result<(), io::Error> {
+        for _ in 0..self.num_events {
+            if !self.handle_event()? {
+                break
+            }
+        }
+        Ok(())
+    }
+
+    /// Wait for an event, then handle it. Return false if we should quit.
+    fn handle_event(&mut self) -> Result<bool, io::Error> {
+        match self.term.next_event() {
+            Some(Ok(Event::MouseEvent(pos))) => {
+                self.paint(pos)?;
+            }
+            Some(Ok(Event::KeyEvent(Key::Char('q')))) => {
+                return Ok(false);
+            }
+            Some(Ok(Event::KeyEvent(Key::Char('c')))) => {
+                self.clear()?;
+            }
+            Some(Ok(Event::KeyEvent(Key::Char('s')))) => {
+                let size = self.term.size()?;
+                self.println(
+                    &format!("size: ({},{})", size.row, size.col),
+                    Style::reverse_color(Color::Magenta),
+                )?;
+            }
+            Some(Ok(Event::KeyEvent(Key::Char(c)))) => {
+                self.println(
+                    &format!("got character: {}", c),
+                    Style::reverse_color(Color::Green),
+                )?;
+            }
+            Some(Ok(Event::KeyEvent(ev))) => {
+                self.println(
+                    &format!("got other key event: {:?}", ev),
+                    Style::reverse_color(Color::Cyan),
+                )?;
+            }
+            Some(Err(err)) => {
+                self.println(
+                    &format!("got error: {:?}", err),
+                    Style::reverse_color(Color::Magenta),
+                )?;
+            }
+            None => {
+                self.println("got no event!", Style::reverse_color(Color::Red))?;
+            }
+        }
+        Ok(true)
+    }
+
+    /// Clear everything that was printed to the terminal.
+    fn clear(&mut self) -> Result<(), io::Error> {
+        self.term.clear()?;
+        self.term.present()?;
+        self.line = 0;
+        Ok(())
+    }
+
+    /// Draw a blue '!' at the given position.
+    fn paint(&mut self, pos: Pos) -> Result<(), io::Error> {
+        self.term
+            .print_char('!', pos, Style::reverse_color(Color::Blue))?;
+        self.term.present()
+    }
+
+    /// Print the text on the next line.
+    fn println(&mut self, text: &str, style: Style) -> Result<(), io::Error> {
+        self.term.print_str(
+            text,
+            Pos {
+                col: 0,
+                row: self.line,
+            },
+            style,
+        )?;
+        self.line += 1;
+        self.term.present()
+    }
+}
