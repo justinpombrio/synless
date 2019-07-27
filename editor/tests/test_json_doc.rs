2396235bc853c501cb9bc3226207855f76daf87a
e-matteson <e.r.matteson@gmail.com>
2019-07-27 17:57:24 -0400

    test: add test for DocPosSpec::CursorAtTop

--- editor/tests/test_json_doc.rs
+++ editor/tests/test_json_doc.rs
@@ -138,6 +138,97 @@ fn test_json_undo_redo() {
 }
 
 #[test]
+fn test_json_cursor_at_top() {
+    let (lang, note_set) = make_json_lang();
+    let name = lang.name().to_string();
+    let lang_set = LanguageSet::new();
+    lang_set.insert(name.clone(), lang);
+    let forest = AstForest::new(&lang_set);
+    let lang = lang_set.get(&name).unwrap();
+    let mut clipboard = Clipboard::new();
+    let mut doc = Doc::new(
+        "MyTestDoc",
+        forest.new_fixed_tree(lang, lang.lookup_construct("root"), &note_set),
+    );
+
+    doc.execute(
+        CommandGroup::Group(vec![Command::TreeNav(TreeNavCmd::Child(0))]),
+        &mut clipboard,
+    )
+    .unwrap();
+
+    doc.execute(
+        CommandGroup::Group(vec![
+            Command::Tree(TreeCmd::Replace(forest.new_flexible_tree(
+                &lang,
+                lang.lookup_construct("list"),
+                &note_set,
+            ))),
+            Command::Tree(TreeCmd::InsertPrepend(forest.new_fixed_tree(
+                &lang,
+                lang.lookup_construct("true"),
+                &note_set,
+            ))),
+            Command::Tree(TreeCmd::InsertAfter(forest.new_fixed_tree(
+                &lang,
+                lang.lookup_construct("false"),
+                &note_set,
+            ))),
+            Command::Tree(TreeCmd::InsertAfter(forest.new_fixed_tree(
+                &lang,
+                lang.lookup_construct("null"),
+                &note_set,
+            ))),
+        ]),
+        &mut clipboard,
+    )
+    .unwrap();
+
+    let test = |doc: &Doc, spec: DocPosSpec, rendered: &str| {
+        let width = 7;
+        let mut window = PlainText::new(Pos {
+            col: width,
+            row: 50,
+        });
+        doc.ast_ref()
+            .pretty_print(width, &mut window.pane().unwrap(), spec, CursorVis::Hide)
+            .unwrap();
+        assert_eq!(window.to_string(), rendered);
+    };
+
+    test(
+        &doc,
+        DocPosSpec::Fixed(Pos::zero()),
+        "[true,\n false,\n null]",
+    );
+    test(&doc, DocPosSpec::CursorAtTop, " null]");
+
+    doc.execute(
+        CommandGroup::Group(vec![Command::TreeNav(TreeNavCmd::Left)]),
+        &mut clipboard,
+    )
+    .unwrap();
+
+    test(&doc, DocPosSpec::CursorAtTop, " false,\n null]");
+
+    doc.execute(
+        CommandGroup::Group(vec![Command::TreeNav(TreeNavCmd::Left)]),
+        &mut clipboard,
+    )
+    .unwrap();
+
+    test(&doc, DocPosSpec::CursorAtTop, "[true,\n false,\n null]");
+
+    doc.execute(
+        CommandGroup::Group(vec![Command::TreeNav(TreeNavCmd::Parent)]),
+        &mut clipboard,
+    )
+    .unwrap();
+
+    test(&doc, DocPosSpec::CursorAtTop, "[true,\n false,\n null]");
+}
+
+#[test]
 fn test_json_string() {
     let (lang, note_set) = make_json_lang();
     let name = lang.name().to_string();
