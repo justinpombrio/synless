197f870e92b9be8bf566369ffa4dc9ef66da7610
Justin <zallambo@gmail.com>
2024-03-23 15:57:27 -0400

    doc: a taste of terminal weirdness

--- examples/crossterm/event.rs
+++ examples/crossterm/event.rs
@@ -4,6 +4,32 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use crossterm::ExecutableCommand;
 use std::io;
 
+// ## Some weird terminal behavior
+//
+// C-shift -- doesn't work, there's no shift modifier and the key comes out lowercase.
+//
+// A-[  --  ignores the next character!
+//
+// Control has weird behavior on many keys:
+//
+//     C-3  ==  Esc
+//     C-8  ==  Backspace
+//     C-i  ==  Tab
+//     C-m  ==  Enter
+//     C-'  ==  \
+//     C-[  ==  Esc
+//     C-2  ==  C-' '
+//     C-`  ==  C-' '
+//     C-\  ==  C-4
+//     C-]  ==  C-5
+//     C-/  ==  C-7
+//
+// The control modifier is lost on the following characters:
+//
+//     = 1 9 ; , .
+//
+// The shift modifier only appears on letters and BackTab.
+
 const HELP: &str = r#"Testing the crossterm crate:
  - ESCAPE to quit!
  - Any other key to display its event.
